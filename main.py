bc4f211835f5a4d610be0907d9843314ffa77a7c
marcoscassone02 <ma.cassone@alumno.um.edu.ar>
2024-07-02 12:39:34 -0300

    chain of responsability

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from absfactory import *
+from chainresponsability import *
+def main(handler):
+    requests = ["request1", "request2", "request3", "request4"]
 
-def main(factory: FurnitureFactory):
-    chair = factory.create_chair()
-    table = factory.create_table()
-
-    print(chair.sit_on())
-    print(table.eat_on())
+    for request in requests:
+        result = handler.handle(request)
+        if result:
+            print(result)
+        else:
+            print(f"{request} was not handled")
 
 if __name__ == "__main__":
-    print("Client: Testing client code with the ModernFurnitureFactory:")
-    main(ModernFurnitureFactory())
+    handler1 = ConcreteHandler1()
+    handler2 = ConcreteHandler2()
+    handler3 = ConcreteHandler3()
 
-    print("\nClient: Testing client code with the VictorianFurnitureFactory:")
-    main(VictorianFurnitureFactory())
+    handler1.set_next(handler2).set_next(handler3)
+
+    print("Client: Sending requests through the chain of handlers:")
+    main(handler1)
